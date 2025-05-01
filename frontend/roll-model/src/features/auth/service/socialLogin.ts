e4d6c8084240cc5479bb759a3f2d7ab5cbd03959
박가희 <930_10@naver.com>
2025-05-02 08:51:55 +0900

    [FE] refactor: 소셜로그인 request 방식 변경

--- frontend/roll-model/src/features/auth/service/socialLogin.ts
+++ frontend/roll-model/src/features/auth/service/socialLogin.ts
@@ -1,31 +1,7 @@
-import { axiosInstance } from "@/shared/lib/axios/axiosInstance";
-
-export const socialLogin = async (provider: "google" | "github") => {
-	try {
-		const response = await axiosInstance.get(`/api/oauth2/authorization/${provider}`, {
-			withCredentials: true,
-		});
-
-		console.log("response:", response);
-
-		const cookieHeader = typeof document !== "undefined" ? document.cookie.split("; ").find((row) => row.startsWith("tokens=")) : null;
-
-		if (!cookieHeader) {
-			throw new Error("토큰 정보를 찾을 수 없습니다.");
-		}
-
-		console.log("cookieHeader:", cookieHeader);
-
-		const tokens = decodeURIComponent(cookieHeader.split("=")[1]);
-		const params = new URLSearchParams(tokens);
-
-		console.log("tokens:", tokens);
-
-		return {
-			accessToken: params.get("accessToken"),
-			refreshToken: params.get("refreshToken"),
-		};
-	} catch (error) {
-		throw error; // 인터셉터는 처리됐지만, 로직상 실패로 인식해야 해서 throw 유지
+export const socialLogin = (provider: "google" | "github") => {
+	if (typeof window !== "undefined") {
+		window.location.href = `/api/oauth2/authorization/${provider}`;
+	} else {
+		throw new Error("OAuth 로그인은 클라이언트 환경에서만 가능합니다.");
 	}
 };
--- frontend/roll-model/src/features/auth/ui/SocialLoginButtons.tsx
+++ frontend/roll-model/src/features/auth/ui/SocialLoginButtons.tsx
@@ -6,32 +6,8 @@ import { socialLogin } from "../service/socialLogin";
 import { Button } from "@/components/ui/button";
 
 export const SocialLoginButtons = () => {
-	const setAccessToken = useSetAtom(accessTokenAtom);
-	const setRefreshToken = useSetAtom(refreshTokenAtom);
-
-	const handleLogin = async (provider: "google" | "github") => {
-		try {
-			const { accessToken, refreshToken } = await socialLogin(provider);
-
-			console.log("accessToken:", accessToken);
-			console.log("refreshToken:", refreshToken);
-
-			if (accessToken && refreshToken) {
-				setAccessToken(accessToken);
-				setRefreshToken(refreshToken);
-				alert("로그인 성공!");
-			} else {
-				throw new Error("토큰 파싱 실패");
-			}
-		} catch (error: unknown) {
-			if (error instanceof Error) {
-				console.error("로그인 에러:", error.message);
-				alert(error.message);
-			} else {
-				console.error("알 수 없는 에러:", error);
-				alert("알 수 없는 에러가 발생했습니다.");
-			}
-		}
+	const handleLogin = (provider: "google" | "github") => {
+		socialLogin(provider);
 	};
 
 	return (
