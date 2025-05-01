58b1d047ca8618bcf911739e8c8e498e1ec13151
임남기 <happynj2697@gmail.com>
2025-05-01 12:21:06 +0900

    [FE] fix: 프로젝트 상세 projectInfo version변수 타입변경

--- frontend/roll-model/src/shared/model/atoms/projectDetail.atoms.ts
+++ frontend/roll-model/src/shared/model/atoms/projectDetail.atoms.ts
@@ -4,7 +4,7 @@ import { ApiProjectDomain } from "@/shared/lib/utils/domainMapping";
 export interface ProjectDetailAtom {
   id: number;
   title: string;
-  version: string;
+  version: number;
   category: "CLASSIFICATION" | "REGRESSION";
   domain: ApiProjectDomain;
   ownerYn: boolean; // 기본값이 false이며, 프로젝트 상세페이지 api로 응답받은 정보 중 ownerYn 값이 true라면, ownerYn값 업데이트
@@ -13,7 +13,7 @@ export interface ProjectDetailAtom {
 const initialProjectDetail: ProjectDetailAtom = {
   id: 0,
   title: "",
-  version: "",
+  version: 1.0,
   category: "CLASSIFICATION",
   domain: "HEALTHCARE",
   ownerYn: false,
