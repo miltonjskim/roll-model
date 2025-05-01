2d79e279b198c1388db3279d676020e2d7932b73
jinwooseok <jinus7949@naver.com>
2025-05-01 19:54:48 +0900

    [BE] fix: project controller api 변경

--- spring/roll_model/src/main/java/com/ccc/roll_model/project/ui/ProjectController.java
+++ spring/roll_model/src/main/java/com/ccc/roll_model/project/ui/ProjectController.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/projects")
+@RequestMapping("/api/projects")
 @RequiredArgsConstructor
 public class ProjectController {
 
