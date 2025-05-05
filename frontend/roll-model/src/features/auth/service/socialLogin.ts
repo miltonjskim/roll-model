273379f187c2a59dd0b7722a5f0271f042984075
박가희 <930_10@naver.com>
2025-05-05 23:01:01 +0900

    [FE] modify: 소셜로그인 이동 url 수정

--- frontend/roll-model/src/features/auth/service/socialLogin.ts
+++ frontend/roll-model/src/features/auth/service/socialLogin.ts
@@ -1,7 +1,7 @@
 export const socialLogin = (provider: "google" | "github") => {
 	if (typeof window !== "undefined") {
 		const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-		window.location.href = `${baseUrl}/api/oauth2/authorization/${provider}`;
+		window.location.href = `${baseUrl}/api/v1/oauth2/authorization/${provider}`;
 	} else {
 		throw new Error("OAuth 로그인은 클라이언트 환경에서만 가능합니다.");
 	}
