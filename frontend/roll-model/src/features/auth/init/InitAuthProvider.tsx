ca508d969c23ffc6b0990807bde6db4b271441f7
박가희 <930_10@naver.com>
2025-05-06 02:43:04 +0900

    [FE] modify: api 요청 url 변경

--- frontend/roll-model/src/features/auth/init/InitAuthProvider.tsx
+++ frontend/roll-model/src/features/auth/init/InitAuthProvider.tsx
@@ -1,27 +1,27 @@
-"use client";
+'use client';
 
-import { useEffect } from "react";
-import { useSetAtom } from "jotai";
-import { isLoggedInAtom, userAtom } from "@/features/auth/model/authAtoms";
-import { axiosInstance } from "@/shared/lib/axios/axiosInstance";
+import { useEffect } from 'react';
+import { useSetAtom } from 'jotai';
+import { isLoggedInAtom, userAtom } from '@/features/auth/model/authAtoms';
+import { axiosInstance } from '@/shared/lib/axios/axiosInstance';
 
 export const InitAuthProvider = () => {
-	const setIsLoggedIn = useSetAtom(isLoggedInAtom);
-	const setUser = useSetAtom(userAtom);
+  const setIsLoggedIn = useSetAtom(isLoggedInAtom);
+  const setUser = useSetAtom(userAtom);
 
-	useEffect(() => {
-		const checkLogin = async () => {
-			try {
-				const res = await axiosInstance.get("/api/me");
-				setUser(res.data); // 유저 정보 저장
-				setIsLoggedIn(true);
-			} catch {
-				setIsLoggedIn(false);
-			}
-		};
+  useEffect(() => {
+    const checkLogin = async () => {
+      try {
+        const res = await axiosInstance.get('/api/v1/auth/members/my', { withCredentials: true });
+        setUser(res.data); // 유저 정보 저장
+        setIsLoggedIn(true);
+      } catch {
+        setIsLoggedIn(false);
+      }
+    };
 
-		checkLogin();
-	}, [setIsLoggedIn, setUser]);
+    checkLogin();
+  }, [setIsLoggedIn, setUser]);
 
-	return null; // 렌더링은 없음
+  return null; // 렌더링은 없음
 };
