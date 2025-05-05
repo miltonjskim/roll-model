ded7411062b736e2c93f0a5613f0b5e0a646ec2e
박가희 <930_10@naver.com>
2025-05-06 02:32:27 +0900

    [FE] modify: api 요청 url 수정

--- frontend/roll-model/src/app/oauth/callback/page.tsx
+++ frontend/roll-model/src/app/oauth/callback/page.tsx
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { useSetAtom } from "jotai";
-import { userAtom, isLoggedInAtom } from "@/features/auth/model/authAtoms";
-import { axiosInstance } from "@/shared/lib/axios/axiosInstance";
-import { showErrorToast } from "@/shared/lib/toast/toast";
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useSetAtom } from 'jotai';
+import { userAtom, isLoggedInAtom } from '@/features/auth/model/authAtoms';
+import { axiosInstance } from '@/shared/lib/axios/axiosInstance';
+import { showErrorToast } from '@/shared/lib/toast/toast';
 
 const CallbackPage = () => {
   const router = useRouter();
@@ -15,23 +15,25 @@ const CallbackPage = () => {
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const response = await axiosInstance.get("/api/me", {
+        const response = await axiosInstance.get('/api/v1/auth/members/my', {
           withCredentials: true,
         });
+        console.log(response);
+
         setUser(response.data);
         setIsLoggedIn(true);
-        router.push("/");
+        router.push('/');
       } catch (error) {
         setIsLoggedIn(false);
-        showErrorToast("로그인 정보 확인에 실패했습니다.")
-        router.push("/");
+        showErrorToast('로그인 정보 확인에 실패했습니다.');
+        router.push('/');
       }
     };
 
     fetchUser();
   }, [router, setUser, setIsLoggedIn]);
 
-  return <div className="text-center py-10 text-xl">로그인 중입니다...</div>;
+  return <div className="py-10 text-center text-xl">로그인 중입니다...</div>;
 };
 
 export default CallbackPage;
