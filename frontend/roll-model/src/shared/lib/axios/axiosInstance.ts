b10610ea2d766e08d3bfa55795b8c22b086e58e2
박가희 <930_10@naver.com>
2025-05-07 08:44:55 +0900

    [FE] refactor: axiosInstance 수정

--- frontend/roll-model/src/shared/lib/axios/axiosInstance.ts
+++ frontend/roll-model/src/shared/lib/axios/axiosInstance.ts
@@ -1,42 +1,48 @@
-import { showErrorToast } from "@/shared/lib/toast/toast";
-import axios from "axios";
+import { showErrorToast } from '@/shared/lib/toast/toast';
+import { ApiResponse } from '@/shared/model/types/apiResponse';
+import axios, { AxiosResponse } from 'axios';
 
 export const axiosInstance = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-	timeout: 10000,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  timeout: 10000,
   withCredentials: true,
-	headers: {
-		"Content-Type": "application/json",
-	},
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 // 요청 인터셉터
 axiosInstance.interceptors.request.use(
-	(config) => {
-		// TODO: 토큰 설정하기
-		// 아래 설정은 예시일 뿐, 이후 백엔드와 말해서 수정할 것
-		const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
-		if (token) {
-			config.headers.Authorization = `Bearer ${token}`;
-		}
-		return config;
-	},
-	(error) => Promise.reject(error)
+  (config) => {
+    // TODO: 토큰 설정하기
+    // 아래 설정은 예시일 뿐, 이후 백엔드와 말해서 수정할 것
+    const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
 );
 
 // 응답 인터셉터
 axiosInstance.interceptors.response.use(
-	(response) => response,
-	(error) => {
-		let message;
-		if (error.response) {
-			message = error.response.data?.error.message;
-			console.error("API Error:", error.response.data?.message || error.message);
-		} else {
-			message = "네트워크 에러가 발생했습니다.";
-			console.error("Network Error:", error.message);
-		}
-		showErrorToast(message);
-		return Promise.reject(error);
-	}
+  (response: AxiosResponse<ApiResponse<unknown>>) => {
+    const apiResponse = response.data;
+
+    if (apiResponse.status !== 200 || apiResponse.error) {
+      const message = apiResponse.error?.message || '알 수 없는 에러가 발생했습니다.';
+      showErrorToast(message);
+      return Promise.reject(new Error(message));
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return apiResponse.data as any;
+  },
+  (error) => {
+    const message = error.response?.data?.error?.message || '네트워크 에러가 발생했습니다.';
+    console.error('API Error:', message);
+    showErrorToast(message);
+    return Promise.reject(error);
+  },
 );
