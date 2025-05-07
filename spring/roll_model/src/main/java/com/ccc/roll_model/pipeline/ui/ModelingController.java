a8205fc246986ef1b6bbcf883861375af995169c
권남희 <knaem.edu@gmail.com>
2025-05-07 11:27:25 +0900

    commit for switch

--- spring/roll_model/src/main/java/com/ccc/roll_model/pipeline/ui/ModelingController.java
+++ spring/roll_model/src/main/java/com/ccc/roll_model/pipeline/ui/ModelingController.java
@@ -1,15 +1,19 @@
 package com.ccc.roll_model.pipeline.ui;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ccc.roll_model.global.utils.ApiUtils;
 import com.ccc.roll_model.pipeline.application.ModelingService;
 import com.ccc.roll_model.pipeline.application.command.ExecuteModelingCommand;
+import com.ccc.roll_model.pipeline.application.command.GetPipelineDatasetInfoCommand;
 import com.ccc.roll_model.pipeline.ui.dto.request.ExecuteModelingRequest;
+import com.ccc.roll_model.pipeline.ui.dto.response.GetPipelineDatasetInfoResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -28,5 +32,16 @@ public class ModelingController {
 			.build()
 		);
 	}
-}
 
+	@GetMapping("/{pipelineId}/dataset/info")
+	public ApiUtils.ApiResponse<GetPipelineDatasetInfoResponse> getPipelineDatasetInfo(@PathVariable String pipelineId, @AuthenticationPrincipal Integer memberId) {
+		GetPipelineDatasetInfoResponse response = modelingService.getPipelineDatasetInfo(
+			GetPipelineDatasetInfoCommand.builder()
+				.memberId(memberId)
+				.pipelineId(pipelineId)
+				.build()
+		);
+
+		return ApiUtils.success(response);
+	}
+}
