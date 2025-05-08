b28ab6f0a829f4b55b9cc4aadca90be11190a2bd
박가희 <930_10@naver.com>
2025-05-08 17:58:08 +0900

    [WIP] feat: PreprocessDataPage 초기 구현 및 uploadedDatasetAtom 연동 테스트

--- frontend/roll-model/src/app/workspace/data-preprocess/page.tsx
+++ frontend/roll-model/src/app/workspace/data-preprocess/page.tsx
@@ -1,5 +1,24 @@
+'use client';
+
+import { uploadedDatasetAtom } from '@/entities/workspace/data-config/workspaceAtoms';
+import { useAtomValue } from 'jotai';
+import { useEffect } from 'react';
+
 const PreprocessDataPage = () => {
-	return <div>PreprocessDataPage</div>;
+  const uploadedData = useAtomValue(uploadedDatasetAtom);
+
+  useEffect(() => {
+    if (!uploadedData) {
+      return;
+    } else {
+      console.log('원본 데이터셋 업로드 응답값:', uploadedData);
+    }
+  }, [uploadedData]);
+  return (
+    <div>
+      <div>{uploadedData?.pipelineId}</div>
+    </div>
+  );
 };
 
 export default PreprocessDataPage;
