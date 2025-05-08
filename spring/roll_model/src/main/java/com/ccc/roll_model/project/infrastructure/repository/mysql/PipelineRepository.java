7c616c1f180c8f058cc6c238e699651135530a62
권남희 <knaem.edu@gmail.com>
2025-05-08 17:48:45 +0900

    feat: 나의 프로젝트 조회 쿼리 개선

--- spring/roll_model/src/main/java/com/ccc/roll_model/project/infrastructure/repository/mysql/PipelineRepository.java
+++ spring/roll_model/src/main/java/com/ccc/roll_model/project/infrastructure/repository/mysql/PipelineRepository.java
@@ -1,6 +1,7 @@
 package com.ccc.roll_model.project.infrastructure.repository.mysql;
 
 import com.ccc.roll_model.project.infrastructure.entity.mysql.PipelineEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -16,12 +17,13 @@ public interface PipelineRepository extends JpaRepository<PipelineEntity, String
     Optional<PipelineEntity> findFirstByProjectIdOrderByRegisteredAtDesc(@Param("projectId") Integer projectId);
 
     // 특정 프로젝트의 삭제되지 않은 파이프라인 중 가장 최신 파이프라인 가져오기
-    @Query("SELECT p FROM PipelineEntity p WHERE p.projectEntity.projectId = :projectId AND p.deletedYn = false ORDER BY p.registeredAt DESC")
+    @Query("SELECT p FROM PipelineEntity p WHERE p.projectEntity.projectId = :projectId AND p.deletedYn = false ORDER BY p.registeredAt DESC limit 1")
     Optional<PipelineEntity> findFirstByProjectIdOrderByRegisteredAtDescAndNotDeletedYn(@Param("projectId") Integer projectId);
 
     // 특정 프로젝트의 삭제되지 않은 상태가 COMPLETED면서 publicYn이 true인 파이프라인 중 가장 최신 파이프라인 가져오기
-    @Query("SELECT p FROM PipelineEntity p WHERE p.projectEntity.projectId = :projectId AND p.deletedYn = false AND p.status = 'COMPLETED' AND p.publicYn = true ORDER BY p.registeredAt DESC")
-    Optional<PipelineEntity> findFirstByProjectIdOrderByRegisteredAtDescAndNotDeletedYnAndStatusIsCompletedAndPublicYn(@Param("projectId") Integer projectId);
+    @Query(value = "SELECT * FROM pipelines p WHERE p.project_id IN (SELECT project_id FROM projects WHERE project_id = :projectId) AND p.deleted_yn = false AND p.status = 'COMPLETED' AND p.public_yn = true ORDER BY p.registered_at DESC LIMIT 1",
+            nativeQuery = true)
+    List<PipelineEntity> findFirstCompletedPublicPipeline(@Param("projectId") Integer projectId);
 
     // 특정 프로젝트의 파이프라인 개수 가져오기
     @Query("SELECT COUNT(p) FROM PipelineEntity p WHERE p.projectEntity.projectId = :projectId")
