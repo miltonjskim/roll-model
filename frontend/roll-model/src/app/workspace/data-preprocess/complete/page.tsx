bb97fb993b2907df4dfd7438d72ff6fe33bcd8c6
박가희 <930_10@naver.com>
2025-05-12 16:53:03 +0900

    [FE] refactor: 모델 학습 페이지로 이동하는 버튼 추가

--- frontend/roll-model/src/app/workspace/data-preprocess/complete/page.tsx
+++ frontend/roll-model/src/app/workspace/data-preprocess/complete/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
 import { completedDatasetAtom } from '@/entities/workspace/data-config/workspaceAtoms';
 import { useAtomValue } from 'jotai';
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 const CompletePreprocessDataPage = () => {
   const completedUploadset = useAtomValue(completedDatasetAtom);
+  const router = useRouter();
 
   console.log('completedUploadset:', completedUploadset);
 
@@ -17,11 +20,18 @@ const CompletePreprocessDataPage = () => {
     }
   }, [completedUploadset]);
 
+  const handleMoveModelingPage = () => {
+    router.push('/workspace/modeling-section');
+  };
   return (
     <div>
       <h1>데이터 전처리 끝</h1>
 
-      <div></div>
+      <div>
+        <Button onClick={handleMoveModelingPage} variant="black">
+          모델 학습하기
+        </Button>
+      </div>
     </div>
   );
 };
