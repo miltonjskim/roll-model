5e6e75d34607380ef4029fdd5e785257044d2c55
박가희 <930_10@naver.com>
2025-05-12 16:48:18 +0900

    [FE] refactor: errorToast message 인수타입에 JSX.Element 추가

--- frontend/roll-model/src/shared/lib/toast/toast.ts
+++ frontend/roll-model/src/shared/lib/toast/toast.ts
@@ -1,9 +1,10 @@
-import { toast } from "sonner";
+import { JSX } from 'react';
+import { toast } from 'sonner';
 
-export const showErrorToast = (message: string = "오류가 발생했습니다.") => {
-	toast.error(message);
+export const showErrorToast = (message: string | JSX.Element = '오류가 발생했습니다.') => {
+  toast.error(message);
 };
 
 export const showSuccessToast = (message: string) => {
-	toast.success(message);
+  toast.success(message);
 };
