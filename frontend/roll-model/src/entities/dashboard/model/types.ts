17183cfdf2ad89dc40765cc5d9742facdb529880
박가희 <930_10@naver.com>
2025-05-13 17:27:21 +0900

    [FE] typo: Project 타입의 rsquared키 값 Api 응답 객체에 맞게 수정

--- frontend/roll-model/src/entities/dashboard/model/types.ts
+++ frontend/roll-model/src/entities/dashboard/model/types.ts
@@ -12,7 +12,7 @@ export interface Project {
   domain: ApiProjectDomain; // 의료 금융 등
   displayDomain?: string; // 화면 표시용 한글 도메인
   accuracy: number | null; // 정확도 (전처리만완료시 accuracy and rSquared null)
-  rSquared: number | null; // 회귀일때의 정확도 (전처리만완료시 accuracy and rSquared null)
+  rsquared: number | null; // 회귀일때의 정확도 (전처리만완료시 accuracy and rSquared null)
   target: string | null; // 목표변수 (전처리만완료시 null)
   dataCount: number; // 데이터수
   runningDuration: number | null; // 학습시간 (전처리만완료시 null)
