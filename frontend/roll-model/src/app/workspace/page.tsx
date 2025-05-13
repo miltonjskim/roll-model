b40d454881489b23e7584365cd67a43796fece9a
박가희 <930_10@naver.com>
2025-05-13 18:00:43 +0900

    [FE] feat: 재학습 선택 시 사용자 보유 프로젝트 목록 조회 페이지로 이동

--- frontend/roll-model/src/app/workspace/page.tsx
+++ frontend/roll-model/src/app/workspace/page.tsx
@@ -1,34 +1,17 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { axiosInstance } from '@/shared/lib/axios/axiosInstance';
-import { showErrorToast } from '@/shared/lib/toast/toast';
-import { ApiError } from '@/shared/model/types/apiResponse';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
 
 const Workspace = () => {
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false);
 
   const handleCreateProject = () => {
     router.push('/workspace/project-meta');
   };
 
-  const fetchMyProject = async () => {
-    // TODO: 내 프로젝트 재학습 목록이 뜨게.. 렌더링 방식은 아직 생각 못함
-    setIsLoading(true);
-
-    try {
-      const response = await axiosInstance('/api/v1/projects/my');
-      console.log('response:', response);
-    } catch (error) {
-      const apiError = error as ApiError;
-      showErrorToast(apiError.message);
-      console.error(apiError);
-    } finally {
-      setIsLoading(false);
-    }
+  const moveToMyProjectListPage = () => {
+    router.push('/workspace/retrain');
   };
 
   return (
@@ -42,7 +25,7 @@ const Workspace = () => {
           <Button variant="black" onClick={handleCreateProject} className="h-full w-1/2 text-lg">
             + 새로운 프로젝트 만들기
           </Button>
-          <Button variant="outline" onClick={fetchMyProject} className="hover:bg-[theme(color-gray-04)] h-full w-1/2 text-lg">
+          <Button variant="outline" onClick={moveToMyProjectListPage} className="hover:bg-[theme(color-gray-04)] h-full w-1/2 text-lg">
             최근 프로젝트에서 재학습
           </Button>
         </div>
