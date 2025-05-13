9086eb5f99d9667a98d9eefae7adae4b002cb3e4
박가희 <930_10@naver.com>
2025-05-14 04:37:59 +0900

    [FE] feat: 샘플데이터셋 목록 조회 API 연동

--- frontend/roll-model/src/app/workspace/data-selection/page.tsx
+++ frontend/roll-model/src/app/workspace/data-selection/page.tsx
@@ -2,13 +2,32 @@
 
 import { Button } from '@/components/ui/button';
 import { FileUploadDialog } from '@/features/workspace/data-upload/ui/FileUploadDialog';
+import { axiosInstance } from '@/shared/lib/axios/axiosInstance';
+import { showErrorToast } from '@/shared/lib/toast/toast';
+import { ApiError } from '@/shared/model/types/apiResponse';
 import { useState } from 'react';
 
 const SelectDataPage = () => {
   const [isHover, setIsHover] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchSampleDatasetList = async () => {
+    setIsLoading(true);
+    try {
+      const { data } = await axiosInstance.get(`/api/v2/datasets/samples`);
+      console.log('response:', data);
+    } catch (error) {
+      const apiError = error as ApiError;
+      showErrorToast(apiError.message);
+      console.error(apiError);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   const handleUseSampleData = () => {
     console.log('샘플 데이터 사용');
+    fetchSampleDatasetList();
   };
 
   const handleUseRegressionData = () => {
@@ -29,7 +48,7 @@ const SelectDataPage = () => {
       <div className="mx-auto mt-4 max-w-[70rem] select-none">
         <div className="bg-[theme(primary-white)] flex h-120 justify-center gap-4 rounded-lg p-4">
           <FileUploadDialog />
-          <div className="h-full flex-1/2 cursor-pointer rounded-lg border border-[var(--color-gray-03)]" onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
+          <div className="h-full flex-1/2 cursor-pointer rounded-lg border border-[var(--color-gray-03)]" onClick={handleUseSampleData}>
             {isHover ? (
               <div className="flex h-full items-center justify-center">
                 <div className="flex h-full w-full flex-col">
