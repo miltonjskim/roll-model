feba5ca2dcefc7e1e3fd7337af4674b1a5207cf0
박가희 <930_10@naver.com>
2025-05-14 11:17:37 +0900

    [FE] style: 공통 레이아웃에 수직 중앙정렬 추가

--- frontend/roll-model/src/app/workspace/layout.tsx
+++ frontend/roll-model/src/app/workspace/layout.tsx
@@ -1,5 +1,5 @@
 const WorkspaceLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
-  return <div className="container mx-auto max-w-[80vw] rounded-md p-4 text-center">{children}</div>;
+  return <div>{children}</div>;
 };
 
 export default WorkspaceLayout;
