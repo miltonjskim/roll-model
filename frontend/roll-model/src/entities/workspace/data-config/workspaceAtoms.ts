6e6b3df6de7a6d6809b6f41e6838c95cfba8069c
박가희 <930_10@naver.com>
2025-05-14 09:45:23 +0900

    [FE] feat: dataset에서 columns 따로 분리

--- frontend/roll-model/src/entities/workspace/data-config/workspaceAtoms.ts
+++ frontend/roll-model/src/entities/workspace/data-config/workspaceAtoms.ts
@@ -1,4 +1,4 @@
-import { CompleteDatasetResponse, UploadDatasetResponse } from '@/entities/workspace/data-config/model/types';
+import { ColumnConfig, CompleteDatasetResponse, UploadDatasetResponse } from '@/entities/workspace/data-config/model/types';
 import { PreviousPreprocessingSteps } from '@/entities/workspace/data-preprocess/model/types';
 import { atom } from 'jotai';
 
@@ -8,3 +8,4 @@ export const uploadedDatasetAtom = atom<UploadDatasetResponse | null>(null);
 export const completedDatasetAtom = atom<CompleteDatasetResponse | null>(null);
 export const pipelineIdAtom = atom<string>('');
 export const preprocessingSteps = atom<PreviousPreprocessingSteps | null>(null);
+export const dataColumns = atom<Record<string, string>[]>([]);
