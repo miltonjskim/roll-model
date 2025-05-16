61ebab80ea534a639f9409ab9ca411d45ebdd96e
박가희 <930_10@naver.com>
2025-05-16 14:00:40 +0900

    [FE] style: loadingspinner 색상 검정에서 흰색으로 변경

--- frontend/roll-model/src/shared/ui/LoadingSpinner.tsx
+++ frontend/roll-model/src/shared/ui/LoadingSpinner.tsx
@@ -1,1 +1,1 @@
-export const LoadingSpinner = () => <div className="h-10 w-10 animate-spin rounded-full border-4 border-black border-t-transparent" />;
+export const LoadingSpinner = () => <div className="h-10 w-10 animate-spin rounded-full border-4 border-white border-t-transparent" />;
